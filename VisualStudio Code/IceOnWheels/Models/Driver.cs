87ac0c806dc2b4bc76f0e441363eb63f1044ecac
François Blanpain <blanpainfrancois@gmail.com>
2017-10-19 18:12:28 +0200

    Start project (angular included) + begin database

---
+++ VisualStudio Code/IceOnWheels/Models/Driver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceOnWheels.Models
+{
+    public class Driver
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int Phonenumber { get; set; }
+        public bool Availibility { get; set; }
+
+    }
+}
