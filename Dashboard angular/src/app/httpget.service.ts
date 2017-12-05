4caf53f0b17a2ea71fb8b2b325a316326ddbc9c8
ClumpsyPenguin <seppe318@gmail.com>
2017-12-05 09:54:19 +0100

    fixed merge conflict issue

--- Dashboard angular/src/app/httpget.service.ts
+++ Dashboard angular/src/app/httpget.service.ts
@@ -6,28 +6,10 @@ import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class HttpgetService {
+  constructor(private http: HttpClient){}
 
-  constructor(private http: Http) {
-  }
-
-  getDrivers(Drivers: any[]) {
-    return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers/');
-
-    constructor(private http: HttpClient)
-    {
-
-    }
-
-  public getDrivers()
-    {
-      return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers');
-      //return this.http.get("https://jsonplaceholder.typicode.com/posts");
-    }
-
-    public getAdressLocation(Location : any[])
-
-    {
-      return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=51.2228618,4.3161537999999995&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAoUXMzvXuuxkZO4JimW1esV6HWvNqdmo0<')
-    }
+  public getDrivers(){
+    return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers');
+    //return this.http.get("https://jsonplaceholder.typicode.com/posts");
   }
 }
