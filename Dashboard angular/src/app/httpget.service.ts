3debfb2ac2a99db4ea6c71a4635a6f3a4b8b7932
ClumpsyPenguin <seppe318@gmail.com>
2017-12-05 09:46:03 +0100

    added dynamic markers

--- Dashboard angular/src/app/httpget.service.ts
+++ Dashboard angular/src/app/httpget.service.ts
@@ -6,19 +6,28 @@ import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class HttpgetService {
-<<<<<<< HEAD
-  constructor(private http: Http){}
-  getDrivers(Drivers:any[]){
-    return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers/');
-=======
-  constructor(private http: HttpClient){}
->>>>>>> aa077df8ac18d64769f35255092e2b36bb573c0d
 
-public getDrivers(){
-    return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers');
-    //return this.http.get("https://jsonplaceholder.typicode.com/posts");
+  constructor(private http: Http) {
   }
-  getAdressLocation(Location:any[]){
-    return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=51.2228618,4.3161537999999995&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAoUXMzvXuuxkZO4JimW1esV6HWvNqdmo0<')
+
+  getDrivers(Drivers: any[]) {
+    return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers/');
+
+    constructor(private http: HttpClient)
+    {
+
+    }
+
+  public getDrivers()
+    {
+      return this.http.get('http://iceonwheels20171117020935.azurewebsites.net/api/drivers');
+      //return this.http.get("https://jsonplaceholder.typicode.com/posts");
+    }
+
+    public getAdressLocation(Location : any[])
+
+    {
+      return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=51.2228618,4.3161537999999995&location_type=ROOFTOP&result_type=street_address&key=AIzaSyAoUXMzvXuuxkZO4JimW1esV6HWvNqdmo0<')
+    }
   }
 }
